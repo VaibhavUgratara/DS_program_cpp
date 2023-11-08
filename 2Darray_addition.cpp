050493fc6d8f1e8b47b88c86d07d488389f077f4
Vaibhav <124518932+VaibhavUgratara@users.noreply.github.com>
2023-11-08 09:36:41 +0530

    Add files via upload

---
+++ 2Darray_addition.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr1[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int arr2[3][3]={{9,8,7},{6,5,4},{3,2,1}};
+    int arr3[3][3];
+
+
+    int r=sizeof(arr1)/sizeof(arr1[0]);
+    int c=sizeof(arr1[0])/sizeof(int);
+
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            arr3[i][j]=arr1[i][j]+arr2[i][j];
+        }
+    }
+    return 0;
+}
