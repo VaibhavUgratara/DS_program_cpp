077d639992f7e0fadf724f7e2aa99d59a53b77c4
Vaibhav <124518932+VaibhavUgratara@users.noreply.github.com>
2023-11-08 10:28:10 +0530

    Update array_sort.cpp

--- array_sort.cpp
+++ array_sort.cpp
@@ -5,11 +5,11 @@ int main() {
     int arr[l]={43,4,6,8,9};
     for(int i=0;i<l-1;i++){
         int key=arr[i+1];
-        for(int j=i+1;j>0;j--){
-            if(key<arr[j-1]){
-                arr[j]+=arr[j-1];
-                arr[j-1]=arr[j]-arr[j-1];
-                arr[j]=arr[j]-arr[j-1];
+        for(int j=i;j>=0;j--){
+            if(key<arr[j]){
+                arr[j+1]+=arr[j];
+                arr[j]=arr[j+1]-arr[j];
+                arr[j+1]=arr[j+1]-arr[j];
             }
             else{
                 break;
