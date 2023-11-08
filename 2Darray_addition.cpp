ffc93badb07a15e96277049f9f9c3ade25ef1a64
Vaibhav <124518932+VaibhavUgratara@users.noreply.github.com>
2023-11-08 09:38:23 +0530

    Update 2Darray_addition.cpp

--- 2Darray_addition.cpp
+++ 2Darray_addition.cpp
@@ -14,5 +14,12 @@ int main(){
             arr3[i][j]=arr1[i][j]+arr2[i][j];
         }
     }
+
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            cout<<arr3[i][j]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
