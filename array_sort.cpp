591674f6708c7de6c637d1ce8ec4d1bac021d0da
Vaibhav <124518932+VaibhavUgratara@users.noreply.github.com>
2024-03-03 20:00:22 +0530

    array_sort.cpp

--- array_sort.cpp
+++ array_sort.cpp
@@ -2,14 +2,13 @@
 using namespace std;
 int main() {
     int l=5;
-    int arr[l]={43,4,6,8,9};
+    int arr[l]={10,9,83,7,1};
     for(int i=0;i<l-1;i++){
         int key=arr[i+1];
         for(int j=i;j>=0;j--){
             if(key<arr[j]){
-                arr[j+1]+=arr[j];
-                arr[j]=arr[j+1]-arr[j];
-                arr[j+1]=arr[j+1]-arr[j];
+                arr[j+1]=arr[j];
+                arr[j]=key;
             }
             else{
                 break;
